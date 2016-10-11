231d73f55a904f750f23ba833652000e0632bcfb
JoelBot <Joel.Lonneker@gmail.com>
2016-10-10 22:26:29 -0400

    some adventure changes before bed

--- aboutme.js
+++ aboutme.js
@@ -9,7 +9,7 @@ var isCityFolk = false
 // - Number of years in college (as Number)
 var totalCollegeYears = 4
 // - High school graduation year (as Number)
-var highSchoolGraduationYear = 1999
+var highSchoolGraduationYear = new Date()
 // - Number of pets (as Number)
 var petCount = 2
 // - Number of children (as Number)
@@ -20,4 +20,13 @@ var favMusicGenre = 'Rock'
 var favMovie = 'Shaun of the Dead'
 // Adventure time!
 
-console.log(firstName, lastName, email, isCityFolk, totalCollegeYears, highSchoolGraduationYear, petCount, childCount, favMusicGenre, favMovie)
+highSchoolGraduationYear.setFullYear(1999)
+console.log('Name: ' + firstName + ' ' + lastName)
+console.log('Email: ' + email)
+console.log('From the city?: ' + isCityFolk)
+console.log('Years in college: ' + totalCollegeYears)
+console.log('Year of high school graduation: ' + highSchoolGraduationYear)
+console.log('Number of pets: ' + petCount)
+console.log('Number of children: ' + childCount)
+console.log('Favorite Music Genre: ' + favMusicGenre)
+console.log('Favorite Movie: ' + favMovie)
